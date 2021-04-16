1eb933f6020a0fddabc76de215f2712f804e834e
Anmol Tandon <75445117+anmol-tandon@users.noreply.github.com>
2021-04-16 14:54:28 +0530

    Delete 3_Implementation directory
    Delete Implementation

--- 3_Implementation/inc/test.h
+++
@@ -1,6 +0,0 @@
-#ifndef __TEST_H__
-#define __TEST_H__
-int rangeOfNumber(int guess);
-
-int StonePaperScissor(char you, char comp);
-#endif
--- 3_Implementation/src/guess.c
+++
@@ -1,6 +0,0 @@
-#include "test.h"
-int rangeOfNumber(int guess)
-{
-return guess;
-}
-
